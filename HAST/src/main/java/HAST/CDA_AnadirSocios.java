56564cdc391a945c76eadf7b5bb5ebf4214c0438
Hely <hely.pereira@ikasle.egibide.org>
2019-05-17 21:50:04 +0200

    Trabajando en añadir socios.

--- HAST/src/main/java/HAST/CDA_AnadirSocios.java
+++ HAST/src/main/java/HAST/CDA_AnadirSocios.java
@@ -3,6 +3,9 @@ package HAST;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class CDA_AnadirSocios {
     private JPanel panel;
@@ -28,6 +31,26 @@ public class CDA_AnadirSocios {
         buttonAnadir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                Connection conexion = BD.getConn();
+
+                String insertarDatosSql = "INSERT INTO DBUSER (nombre, apellido, DNI, telefono, email, perfil) VALUES (?,?,?,?,?,?)";
+
+
+                try {
+
+                    PreparedStatement anyadirSocio = conexion.prepareStatement(insertarDatosSql);
+                    anyadirSocio.setString(1, textNombre.getText());
+                    anyadirSocio.setString(2, textApellidos.getText());
+                    anyadirSocio.setString(3, textDNI.getText());
+                    anyadirSocio.setString(4, textTelefono.getText());
+                    anyadirSocio.setString(5, textEmail.getText());
+                    anyadirSocio.setString(6, selectorPerfil.getModel().toString());
+
+
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
 
             }
         });
