9bee976e5149e98d4a2c135091829fd1ec5987e9
Hely <hely.pereira@ikasle.egibide.org>
2019-05-24 21:09:30 +0200

    Editar Socio, pero no actualiza

--- HAST/src/main/java/HAST/BD.java
+++ HAST/src/main/java/HAST/BD.java
@@ -30,9 +30,9 @@ public class BD {
 
             // Cadena de conexión: driver@machineName:port:SID, userid, password
 
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "egibide", "egibide");
+            //conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "egibide", "egibide");
             //El de abajo Hely
-            //conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.56.101:1521:xe", "egibide", "egibide");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.56.101:1521:xe", "egibide", "egibide");
 
             System.out.println("INFO: Conexión abierta");
 
